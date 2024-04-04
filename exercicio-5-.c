7b1a8188144740aa8844365036b2286d6ade18ed
roger-ti <165339065+roger-ti@users.noreply.github.com>
2024-04-04 19:39:04 -0300

    Add files via upload

---
+++ exercicio-5-.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    
+    int num;
+    printf("Digite um numero de 1 a 12");
+    scanf("%i", &num);
+    return 0;
+}
