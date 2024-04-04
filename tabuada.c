45e0f81a38f379347d3802bdb61b7ab18b8f2226
roger-ti <165339065+roger-ti@users.noreply.github.com>
2024-04-04 19:11:15 -0300

    Add files via upload

---
+++ tabuada.c
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int numero;
+
+    printf("Digite um número entre 1 e 10: ");
+    scanf("%d", &numero);
+
+    
+    
+    printf("%d X 1 = %d\n", numero, numero * 1);
+    printf("%d X 2 = %d\n", numero, numero * 2);
+    printf("%d X 3 = %d\n", numero, numero * 3);
+    printf("%d X 4 = %d\n", numero, numero * 4);
+    printf("%d X 5 = %d\n", numero, numero * 5);
+    printf("%d X 6 = %d\n", numero, numero * 6);
+    printf("%d X 7 = %d\n", numero, numero * 7);
+    printf("%d X 8 = %d\n", numero, numero * 8);
+    printf("%d X 9 = %d\n", numero, numero * 9);
+    printf("%d X 10 = %d\n", numero, numero * 10);
+
+    return 0;
+}
+
